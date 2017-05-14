7469164ed8af587c5b1b4386e23acf6e7e39cfb6
freshhh <bgayle786@gmail.com>
2017-05-13 21:48:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
 
-echo 'love you';
+echo 'love you. This is fun';
+
+$favArtists = ["Lil Uzi Vert","Lil Wayne","Chance The Rapper", "XXXtentacion","Lil Yachty", "Ugly God"];
+echo $favArtists[rand(0,$favArtists.length())];
 
 ?>
